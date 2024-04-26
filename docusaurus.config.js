79472a52b812a001daab6e33d8f6bb7c6f27cee1
Somos Talento Azteca <somostalentoazteca@proton.me>
2024-04-25 22:32:27 -0600

    Usar rutas del proyecto

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,14 +49,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Somos-Talento-Azteca/docs-global/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Somos-Talento-Azteca/docs-global/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -108,7 +108,7 @@ const config = {
             items: [
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/Somos-Talento-Azteca',
               },
               // {
               //   label: 'Discord',
